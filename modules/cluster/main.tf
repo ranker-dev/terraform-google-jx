51052bac08831ba6313e3d0aea61ee56a0053680
Skylar Wolf <skylar@ranker.com>
2021-03-07 21:13:34 -0800

    enable private endpoint

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -62,7 +62,7 @@ resource "google_container_cluster" "jx_cluster" {
 
   private_cluster_config {
     enable_private_nodes = true
-    enable_private_endpoint = false
+    enable_private_endpoint = true
     master_ipv4_cidr_block = "172.16.4.0/28"
   }
 
